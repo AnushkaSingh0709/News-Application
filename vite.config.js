90919de31769104f3c0793292996ca50d6ee87d5
AnushkaSingh0709 <0709anushkasingh@gmail.com>
2024-10-16 00:02:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/News-Application/', // The name of your GitHub repository
   plugins: [react()],
 })
